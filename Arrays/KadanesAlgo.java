295de3e53f653cc2901042105e7af44d306d5ad9
k-tanuj <tanujkumawat3008@gmail.com>
2026-08-10 19:02:17 +0530

    kadanes algo for max subarray sum

---
+++ Arrays/KadanesAlgo.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class KadanesAlgo {
+    public static void main(String[] args) {
+        
+        int [] arr = {-2,-3,4,-1,-2,1,5,-3} ;
+        
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            currSum = currSum + arr[i];
+            if(currSum<0)
+                currSum = 0;
+
+            maxSum = Math.max(currSum,maxSum);
+
+        }
+        System.out.println("Maximum Subarray sum is: "+ maxSum);
+    } 
+}
