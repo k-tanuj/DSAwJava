1cc267025442b88c19f1d19edc8505a8841b04df
k-tanuj <tanujkumawat3008@gmail.com>
2026-08-10 19:56:34 +0530

    return indices of the elements that matches the sum of element and target

---
+++ Arrays/twoSum.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class twoSum {
+
+    public static int[] TwoSum(int[] arr, int target) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] + arr[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 4, 5, 6, 8, -8};
+        int target = 0;
+
+        int[] result = TwoSum(arr, target);
+
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
