a9edc49d39ac07880544fc272d170e193bb8e78b
k-tanuj <tanujkumawat3008@gmail.com>
2026-08-10 20:04:04 +0530

    find duplicate values in array and return true or false

---
+++ Arrays/DuplicateValues.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class DuplicateValues {
+    public static boolean Duplicate(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 4,2, 5, 6, 8, -8};
+        int target = 0;
+        boolean result = Duplicate(arr);
+
+        System.out.println(result);
+    }
+}
