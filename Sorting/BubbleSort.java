1b85f616085988884a7c00f74015d83e72fac5dc
k-tanuj <tanujkumawat3008@gmail.com>
2026-08-16 09:14:02 +0530

    bubble

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,21 @@
+public class BubbleSort {
+    public static void main(String[] args){
+        int[] arr = {2, 3, 1, 9, 7, 6};
+        int n = arr.length;
+
+        for(int i =0;i<n;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted array:");
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+
+    }
+}
