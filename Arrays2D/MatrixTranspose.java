480739fcc387290e4c5d3a48bbef71ae1b03b73f
k-tanuj <tanujkumawat3008@gmail.com>
2026-08-25 21:55:02 +0530

    matrix transpose

---
+++ Arrays2D/MatrixTranspose.java
@@ -0,0 +1,45 @@
+package Arrays2D;
+
+public class MatrixTranspose {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+        System.out.println("initial matrix:");
+        printMatrix(matrix);
+
+        int[][] transpose = new int[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                transpose[j][i] = matrix[i][j];
+
+            }
+        }
+
+        printMatrix(transpose);
+    }
+
+    public static void printMatrix(int[][] matrix) {
+
+        System.out.println("The transpose Matrix is:");
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                System.out.print(matrix[i][j] + " ");
+
+            }
+
+            System.out.println();
+        }
+    }
+}
