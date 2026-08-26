d975a6c8f07819ef65bed0333d13aaa90b057c2e
k-tanuj <tanujkumawat3008@gmail.com>
2026-08-26 22:11:12 +0530

    set zeroes

---
+++ Arrays2D/SetZeroes.java
@@ -0,0 +1,50 @@
+package Arrays2D;
+
+public class SetZeroes {
+
+    public void setZeroes(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] mat = {
+            {0, 2, 3, 0},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {0, 14, 15, 0}
+        };
+
+        SetZeroes obj = new SetZeroes();
+        obj.setZeroes(mat);
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
